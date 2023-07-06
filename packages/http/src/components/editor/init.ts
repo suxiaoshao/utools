a81d548f7a0785e1c3c72daa915c506ccb60829f
Su Weijie <weijie.su@shopee.com>
2023-07-06 17:11:50 +0800

    feat(http): 🎸 response body

--- packages/http/src/components/editor/init.ts
+++ packages/http/src/components/editor/init.ts
@@ -1,20 +1,22 @@
 import 'monaco-editor/esm/vs/editor/editor.main';
 
-const tsWorker = new Worker('./tsWorker.js');
-const cssWorker = new Worker('./cssWorker.js');
-const htmlWorker = new Worker('./htmlWorker.js');
-const jsonWorker = new Worker('./jsonWorker.js');
-const editorWorker = new Worker('./editorWorker.js');
-
 self.MonacoEnvironment = {
-  getWorker(_: string, label: string) {
-    if (label === 'typescript' || label === 'javascript') return tsWorker;
-    if (label === 'json') return jsonWorker;
-    if (label === 'css') return cssWorker;
-    if (label === 'html') return htmlWorker;
-    return editorWorker;
+  getWorkerUrl: function (_, label: string) {
+    if (label === 'json') {
+      return './json.worker.js';
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return './css.worker.js';
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return './html.worker.js';
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return './ts.worker.js';
+    }
+    return './editor.worker.js';
   },
-  createTrustedTypesPolicy() {
-    return undefined;
-  },
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  createTrustedTypesPolicy: self.MonacoEnvironment?.createTrustedTypesPolicy,
 };
--- packages/http/src/page/NewWork/Response/components/Success/index.tsx
+++ packages/http/src/page/NewWork/Response/components/Success/index.tsx
@@ -4,6 +4,7 @@ import { Box } from '@mui/material';
 import { useMemo } from 'react';
 import ResponseToggle from './ResponseToggle';
 import Headers from './Headers';
+import Body from './Body';
 
 export interface SuccessProps {
   data: SuccessResponse;
@@ -13,13 +14,13 @@ export default function Success({ data }: SuccessProps) {
   const content = useMemo(() => {
     switch (data.tab) {
       case ResponseTab.body:
-        return <>body</>;
+        return <Body data={data.body} />;
       case ResponseTab.cookie:
         return <>cookie</>;
       case ResponseTab.header:
         return <Headers headers={data.headers} />;
     }
-  }, [data.headers, data.tab]);
+  }, [data.body, data.headers, data.tab]);
   return (
     <Box sx={CommonStyle.main}>
       <ResponseToggle />
